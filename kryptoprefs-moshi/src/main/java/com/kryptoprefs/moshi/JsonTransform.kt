1246d80ac68cfc7a6cc8547f580b853a69c989d9
rumboalla <rumboalla@users.noreply.github.com>
2019-11-28 19:02:12 +0100

    Added kryptoprefs-moshi

--- kryptoprefs-moshi/src/main/java/com/kryptoprefs/moshi/JsonTransform.kt
+++ kryptoprefs-moshi/src/main/java/com/kryptoprefs/moshi/JsonTransform.kt
@@ -4,7 +4,9 @@ import com.kryptoprefs.context.transform.Transform
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
-class JsonTransform<T: Any>(type: Class<T>, moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()): Transform<T> {
+var defaultMoshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+class JsonTransform<T: Any>(type: Class<T>, moshi: Moshi = defaultMoshi): Transform<T> {
 	private val adapter = moshi.adapter(type)
 	override fun transform(t: T?): String? = adapter.toJson(t)
 	override fun transform(t: String?): T? = t?.let { adapter.fromJson(t) }
